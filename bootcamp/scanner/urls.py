ddbdc59d415566358bf39f464f71e7363ed32d6b
Suraj M S <braindeadpool@gmail.com>
2017-03-27 00:59:27 -0400

    scanner system base works

--- bootcamp/scanner/urls.py
+++ bootcamp/scanner/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from bootcamp.scanner import views
 
 urlpatterns = [
+    url(r'^$', views.scan_main, name='scan_main'),
     url(r'^device/(?P<identification>\w+)/$', views.scan_device, name='scan_device'),
-    url(r'^user/(?P<identification>\w+)/$', views.scan_user, name='scan_user    '),
+    url(r'^project/(?P<identification>\w+)/(?P<project_name>[-\w]+)/$', views.scan_project, name='scan_project'),
+    url(r'^user/(?P<identification>\w+)/$', views.scan_user, name='scan_user'),
 ]
--- bootcamp/scanner/views.py
+++ bootcamp/scanner/views.py
@@ -9,6 +9,10 @@ from bootcamp.feeds.models import Feed
 from bootcamp.feeds.views import FEEDS_NUM_PAGES, feeds
 
 
+def scan_main(request):
+    return render(request, 'scanner/wizard.html')
+
+
 def scan_device(request, identification):
     device = get_object_or_404(Device, identification=identification)
     request.user = User.objects.get(username='scanner')
@@ -18,19 +22,20 @@ def scan_device(request, identification):
 def scan_user(request, identification):
     profile = get_object_or_404(Profile, identification=identification)
     page_user = profile.user
-    all_feeds = Feed.get_feeds().filter(user=page_user)
     all_projects = Project.get_published_by_user(page_user)
-    print page_user
-    print all_projects
-    paginator = Paginator(all_feeds, FEEDS_NUM_PAGES)
-    feeds = paginator.page(1)
-    from_feed = -1
-    if feeds:
-        from_feed = feeds[0].id
-    return render(request, 'core/profile.html', {
+    return render(request, 'scanner/projects.html', {
+        'identification': identification,
         'page_user': page_user,
-        'feeds': feeds,
-        'from_feed': from_feed,
-        'page': 1,
         'projects': all_projects,
+    })
+
+
+def scan_project(request, identification, project_name):
+    project = get_object_or_404(Project, slug=project_name)
+    profile = get_object_or_404(Profile, identification=identification)
+    page_user = profile.user
+    return render(request, 'scanner/samples.html', {
+          'identification': identification,
+          'page_user': page_user,
+          'project': project,
     })
